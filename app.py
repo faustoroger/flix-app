5bbf472c4ec22a0512879962ff204c4e3db79b60
Fausto Roger <hixian@gmail.com>
2025-11-29 19:40:42 -0300

    feat: Added menu with options

---
+++ app.py
@@ -0,0 +1,31 @@
+import streamlit as st
+
+
+def main():
+    st.title("Flix App")
+
+    menu_option = st.sidebar.selectbox(
+        label="Selecione uma opção",
+        options=[
+            "Início",
+            "Gêneros",
+            "Atores/Atrizes",
+            "Filmes",
+            "Avaliações",
+        ],
+    )
+
+    if menu_option == "Início":
+        st.write("Início")
+    if menu_option == "Gêneros":
+        st.write("Lista de Gêneros")
+    if menu_option == "Atores/Atrizes":
+        st.write("Lista de Atores/Atrizes")
+    if menu_option == "Filmes":
+        st.write("Lista de Filmes")
+    if menu_option == "Avaliações":
+        st.write("Lista de Avaliações")
+
+
+if __name__ == "__main__":
+    main()
