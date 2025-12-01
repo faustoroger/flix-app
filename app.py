b596e8ae6f229d781d692dcbfb05bdcd1f7c473a
Fausto Roger <hixian@gmail.com>
2025-12-01 20:36:01 -0300

    feat(app): Integrate movie and review display functionality in main app

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import streamlit as st
 
 from actors.page import show_actors
 from genres.page import show_genres
+from movies.page import show_movies
+from reviews.page import show_reviews
 
 
 def main():
@@ -25,9 +27,9 @@ def main():
     if menu_option == "Atores/Atrizes":
         show_actors()
     if menu_option == "Filmes":
-        st.write("Lista de Filmes")
+        show_movies()
     if menu_option == "Avaliações":
-        st.write("Lista de Avaliações")
+        show_reviews()
 
 
 if __name__ == "__main__":
