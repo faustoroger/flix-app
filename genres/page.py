18c391061f20428f926a1629f41e7294e58642a6
Fausto Roger <hixian@gmail.com>
2025-12-12 10:34:22 -0300

    feat(genres): Refactor genre management by implementing GenreService for better structure

--- genres/page.py
+++ genres/page.py
@@ -2,31 +2,30 @@ import pandas as pd
 import streamlit as st
 from st_aggrid import AgGrid
 
-genres = [
-    {
-        "id": 1,
-        "name": "Ação",
-    },
-    {
-        "id": 2,
-        "name": "Comédia",
-    },
-    {
-        "id": 3,
-        "name": "Terror",
-    },
-]
+from genres.service import GenreService
 
 
 def show_genres():
-    st.write("Lista de Gêneros")
+    genre_service = GenreService()
+    genres = genre_service.get_genres()
 
-    AgGrid(
-        data=pd.DataFrame(genres),
-        key="genres_grid",
-    )
+    if genres:
+        st.write("Lista de Gêneros:")
+        genres_df = pd.json_normalize(genres)
+        AgGrid(
+            data=genres_df,
+            key="genres_grid",
+        )
+    else:
+        st.warning(body="Nenhum gênero encontrado.")
 
     st.title(body="Cadastrar novo Gênero")
     name = st.text_input(label="Nome do Gênero")
     if st.button(label="Cadastrar"):
-        st.success(f'Gênero "{name}" cadastrado com sucesso')
+        new_genre = genre_service.create_genre(
+            name=name,
+        )
+        if new_genre:
+            st.rerun()
+        else:
+            st.error(body="Erro ao cadastrar o gênero. Verifique os campos.")
